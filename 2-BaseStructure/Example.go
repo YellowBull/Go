611b181a6514a41d2cbc50dd5e43f930b4be6f9e
airvicii <782294559@qq.com>
2018-01-09 16:42:11 +0800

    create Example.go

---
+++ 2-BaseStructure/Example.go
@@ -0,0 +1,31 @@
+//程序包名
+package main
+
+//导入的包名
+import (
+	"fmt"
+)
+
+//常量定义
+const (
+	PI=3.14
+)
+
+//全局变量定义
+var (
+	name = "xxx"
+)
+
+//一般类型声明
+type number int
+
+//结构声明
+type myStruct struct{}
+
+//接口声明
+type myInterface interface{}
+
+//主函数
+func main() {
+	fmt.Println("Hello word 你好世界")
+}
